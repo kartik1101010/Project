19c18969bc763882797ad7ccfa65ce4ccd8c6498
Bhargava Malladi <krishna300802@gmail.com>
2025-04-20 11:14:25 -0700

    Setup

---
+++ setup_init.bat
@@ -0,0 +1,35 @@
+@echo off
+rem -----------------------------------------
+rem Project: Wall Climbing Robot
+rem Description: A wall climbing robot using magnetic adhesion
+rem -----------------------------------------
+
+rem -----------------------------------------
+rem Skills: Arduini IDE, PCB Design, Electronics, Power Management, Eagle Software 
+rem Requirements: Arduino UNO, Arduino IDE, Servos, Relays, Firmware
+rem -----------------------------------------
+
+echo Project: Wall Climbing Robot
+echo Init
+
+rem -----------------------------------------
+rem Step 1: Init a workspace
+rem -----------------------------------------
+
+mkdir workspace
+
+rem -----------------------------------------
+rem Step 2: Instructions
+rem -----------------------------------------
+
+echo Always pull before working.
+echo Push after work is done
+
+rem -----------------------------------------
+rem Step 3: Download KiCAD PCB Design
+rem -----------------------------------------
+
+echo Download KiCAD using https://www.kicad.org/download/
+echo Download for your operating system
+
+rem -----------------------------------------
